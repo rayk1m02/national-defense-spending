51692ef1ea9ea4ad5810a1bb245b0c4ca49bb812
Raymond Kim <hkapray@gmail.com>
2026-09-19 12:09:09 -0500

    starting files, viewing sample data

---
+++ sample_data.py
@@ -0,0 +1,6 @@
+#%%
+import pandas as pd
+df = pd.read_csv(r"C:\Users\rkim\Desktop\Learning\national-defense-spending\bulk_files_sample\All_Contracts_Prime_2026_1.csv", nrows=20)
+df.columns.tolist()
+
+# %%
